140de76eee4770cacf5a7184ea54e2235d3ee11a
Andrew Zharov <crossfirezharov@gmail.com>
2022-03-16 23:02:47 +0500

    Initial Commit

---
+++ happit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  happit
+//
+//  Created by Андрей  on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
